e7abbbaeb3a84e8659ef6ed8f058a89b552b435a
Sleepwalkerqwe <anatik2005@gmail.com>
2025-01-26 23:05:59 +0100

    Calculating distance

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -28,6 +28,8 @@ router
 // /tours-distance?distance=233&center=-40, 45&unit=mi
 // /tours-distance/233/center/-40,45/unit/mi
 
+router.route('/distances/:latlng/unit/:unit').get();
+
 router
   .route('/:id')
   .get(tourController.getTour)
