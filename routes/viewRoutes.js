c720eb4840e8eb448f658fd62ba4cdb0be646d52
Sleepwalkerqwe <anatik2005@gmail.com>
2025-01-29 11:06:31 +0100

    build the tour overview page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,7 +3,7 @@ const viewsController = require('../controllers/viewsController');
 
 const router = express.Router();
 
-module.exports = router;
-
 router.get('/', viewsController.getOverview);
 router.get('/tour', viewsController.getTour);
+
+module.exports = router;
