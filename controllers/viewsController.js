fd09470bbad8ef95f7792c778119a8f2a632f149
Sleepwalkerqwe <anatik2005@gmail.com>
2025-01-30 23:29:12 +0100

    created tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -13,8 +14,21 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  const tourReivews = await Review.find({ tour: tour._id });
+
+  const tourGuides = tour.guides;
+  // create route and get the data
+  // 2) Build template
+  // 3) Render template using the data from 1)
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
