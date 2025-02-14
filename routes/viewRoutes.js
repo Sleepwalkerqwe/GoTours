0973a1dc38f685628077cf969b2d0762b8104f39
Sleepwalkerqwe <anatik2005@gmail.com>
2025-02-14 22:53:43 +0100

    logging with users api part 2

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,9 +3,10 @@ const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
+// router.use(authController.protect);
 
 router.get('/', viewsController.getOverview);
-router.get('/tour/:slug', authController.protect, viewsController.getTour);
+router.get('/tour/:slug', viewsController.getTour);
 
 router.get('/login', viewsController.getLoginForm);
 
