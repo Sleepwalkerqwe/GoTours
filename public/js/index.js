25168bf759c97331d627142c87fa32e8aba46ffd
Sleepwalkerqwe <anatik2005@gmail.com>
2025-03-03 16:28:49 +0100

    finishing the bookings API, add signup

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,16 @@
 /* eslint-disable*/
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login, logout } from './login';
+import { login, logout, signup } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alert';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
+const signupForm = document.querySelector('.form--signup');
+
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
@@ -27,6 +30,23 @@ if (loginForm) {
     const password = document.getElementById('password').value;
 
     login(email, password);
+  });
+}
+
+if (signupForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // VALUES
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+
+    if (password !== passwordConfirm) {
+      showAlert('error', 'Passwords must be same');
+    } else {
+      signup(name, email, password, passwordConfirm);
+    }
   });
 }
 
